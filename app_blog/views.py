58864828e4a634b91fa7e5ee09355b3fac0d565f
Haxxproxx202 <pjanecki88@gmail.com>
2022-10-02 11:15:25 +0200

    tags done

--- app_blog/views.py
+++ app_blog/views.py
@@ -4,22 +4,29 @@ from .forms import EmailPostForm, CommentForm
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from django.core.mail import send_mail
+from taggit.models import Tag
+from django.db.models import Count
 
+def post_list(request, tag_slug=None):
+    object_list = Post.published.all()
+    tag = None
 
-# def post_list(request):
-#     object_list = Post.published.all()
-#     paginator = Paginator(object_list, 3)
-#     page = request.GET.get('page')
-#     try:
-#         posts = paginator.page(page)
-#     except PageNotAnInteger:
-#         posts = paginator.page(1)
-#     except EmptyPage:
-#         posts = paginator.page(paginator.num_pages)
-#
-#     return render(request,
-#                   'app_blog/post/list.html', {'posts': posts,
-#                                               'page': page})
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+    paginator = Paginator(object_list, 3)
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+
+    return render(request,
+                  'app_blog/post/list.html', {'posts': posts,
+                                              'page': page,
+                                              'tags': tag})
 
 class PostListView(ListView):
     queryset = Post.published.all()
@@ -45,11 +52,17 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    print(post_tags_ids)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
 
+    print(post_tags_ids)
     return render(request, 'app_blog/post/detail.html', {'post': post,
                                                          'comments': comments,
                                                          'comment_form': comment_form,
-                                                         'new_comment': new_comment})
+                                                         'new_comment': new_comment,
+                                                         'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):
